ec829f40e2fb7cd37f35d5ad4b8b7e9044720a4f
atLAhs2000 <alyssalahoy@gmail.com>
2022-09-17 19:48:00 -0700

    change to process port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
 });
