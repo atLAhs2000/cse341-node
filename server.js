58e1c4cb1eb5651e798661096cf90814e3c214c7
atLAhs2000 <alyssalahoy@gmail.com>
2022-09-17 19:56:52 -0700

    change port position in script

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
