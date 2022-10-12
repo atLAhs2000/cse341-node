bf468a437f2ce8f5154cfafe3bfdde2d8a942206
atLAhs2000 <alyssalahoy@gmail.com>
2022-10-11 21:28:05 -0700

    add dotenv config to server file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const connectDB = require('./DB/connect');
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
