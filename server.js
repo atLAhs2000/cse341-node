8796c43a53752926182a3a0df121ef34d1cf5489
atLAhs2000 <alyssalahoy@gmail.com>
2022-10-15 22:53:57 -0700

    change set header for cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app
     .use(bodyParser.json())
     .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Headers", "Authorization, Cache-Control, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         next();
     })
     .use('/', require('./routes'));
